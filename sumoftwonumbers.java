d571d4620d886bf13627d9a4d85e5bfb6c5288d5
deepika055 <78158452+deepika055@users.noreply.github.com>
2022-11-21 18:30:01 +0530

    Add files via upload

---
+++ sumoftwonumbers.java
@@ -0,0 +1,17 @@
+package basic;
+import java.util.*;
+
+public class Sumoftwonumbers {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		int sum= a+b;
+		System.out.println(sum);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
